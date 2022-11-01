1db2bb78a823bef58bf17ee450607f25bec3e873
Kris Tung <ragdoll.washoku89@gmail.com>
2022-11-01 22:25:51 +0800

    git init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view />
+  {{name}}
+  <button type="button" class="btn btn-primary">Primary</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: process.env.VUE_APP_NAME
+      // process.env.為引入環境變數的固定寫法
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "bootstrap";
+</style>
